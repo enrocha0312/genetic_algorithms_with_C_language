9063f0c0b3dc7836cab70a7491d6a16366a002c4
enrocha0312 <enrocha0312@gmail.com>
2023-03-08 14:46:07 -0300

    indexAssociate

--- BinaryGeneticAlgorithms/GeneticAlgorithmsLibrary.h
+++ BinaryGeneticAlgorithms/GeneticAlgorithmsLibrary.h
@@ -21,3 +21,37 @@ Cromossomo * chromossomeAlloc(int size)
     return auxiliar;
 }
 
+int * indexAssociate(int binaryVector[])
+{
+	int size = sizeof(binaryVector)/sizeof(int);
+	int *indexes = intAlloc(size);
+	int auxVector [size];
+	
+	for(int k=0; k<size; k++)
+	{
+		auxVector[k] = binaryVector[k];	
+	}
+	int temp;
+	for (int i =0; i<size; ++i){
+		for(int j=  i+1; j<size; ++j){
+			if(auxVector[i]<auxVector[j]){
+				temp = auxVector[i];
+				auxVector[i]= auxVector[j];
+				auxVector[j]= temp;
+			}
+		}
+	}	
+	for (int m = 0; m<tam; m++ )
+	{
+		for(int n=0; n<tam; n++)
+		{
+			if (auxVector[m] == binaryVector[n])
+			{
+				indexes[m] = n;
+				break;
+			}
+		}
+	}
+	return indexes;
+}
+
